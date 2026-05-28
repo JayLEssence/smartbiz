53e93067e28164ff02a60286a47ed44e1cac4042
Z User <z@container>
2026-05-28 19:04:51 +0000

    50075ca7-3836-4ab4-959a-297da7572fe5

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,8 +37,8 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
-      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+      { url: "/icon-glossy-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-glossy-512.png", sizes: "512x512", type: "image/png" },
     ],
     apple: [{ url: "/apple-touch-icon.png" }],
   },
